a2e584f666c2f721fe3075aec70d75564b21469d
Michael Malto <michaeldm233@gmail.com>
2021-04-12 18:31:09 +0100

    modified the config for host and port

--- source/config/config.ts
+++ source/config/config.ts
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 8080;
+const HOSTNAME = process.env.HOSTNAME || '0.0.0.0';
+const PORT = process.env.SERVER_PORT || 8080;
 
 const SERVER = {
-    hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT
+    hostname: HOSTNAME,
+    port: PORT
 };
 
 const config = {
